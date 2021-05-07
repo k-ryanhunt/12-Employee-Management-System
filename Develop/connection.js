d97d3d3f34d889e54d72862ede7a8267579c8b64
K Ryan <hunt.kelsearyan@gmail.com>
2021-05-06 18:28:27 -0600

    Connection.js planned out

---
+++ Develop/connection.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+const util = require('util');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Ch@rlie10311989',
+    database: 'employeesDB'
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`Connected as ID ${connection.threadId}\n`);
+});
+
+module.exports = connection
