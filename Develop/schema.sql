71e75ccece65b9a9413c8fd3f259a8fadce9a65a
K Ryan <hunt.kelsearyan@gmail.com>
2021-05-06 18:33:18 -0600

    Updated the FOREIGN KEY and REFERENCES in schema.sql

--- Develop/schema.sql
+++ Develop/schema.sql
@@ -5,18 +5,21 @@ CREATE DATABASE employeesDB;
 USE employeesDB;
 
 CREATE TABLE department (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
 );
 
 
 CREATE TABLE employee (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT NULL
+    manager_id INT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role (id),
+    FOREIGN KEY (manager_id) REFERENCES employee (id),
 );
 
 
@@ -25,5 +28,6 @@ CREATE TABLE role (
     title VARCHAR(30) NULL,
     salary DECIMAL(10,4) NULL,
     department_id INT NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department (id),
 );
